a228ccd0d9514395e7d232c151bb435e182d443d
raam1432 <onlinecode1137@gmail.com>
2025-06-02 09:05:38 -0700

    Update urls.py

--- setu_coin/urls.py
+++ setu_coin/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
 ]
+
